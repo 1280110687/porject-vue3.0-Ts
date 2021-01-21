adbdffa87a014941e482f4ffeb8a7e8bc62b116b
xiaoming <xmxm@kg.com>
2021-01-21 14:04:56 +0800

    init
    porject init

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,11 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+
+import state from "./modules/state";
+import mutations from "./modules/mutations";
+import actions from "./modules/actions"
 
 export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+  state,
+  mutations,
+  actions
 })
